03515a14b334073589a476afd96620e253305cfa
ruben abarca <abarca.ruben73@gmail.com>
2015-04-19 23:38:30 -0600

    Init project, gulp

---
+++ src/client/app/dashboard/dashboard.route.spec.js
@@ -0,0 +1,29 @@
+/* jshint -W117, -W030 */
+describe('dashboard', function() {
+    describe('state', function() {
+        var controller;
+        var views = {
+            dashboard: 'app/dashboard/dashboard.html'
+        };
+
+        beforeEach(function() {
+            module('app.dashboard', bard.fakeToastr);
+            bard.inject(this, '$location', '$rootScope', '$state', '$templateCache');
+            $templateCache.put(views.dashboard, '');
+        });
+
+        it('should map / route to dashboard View template', function() {
+            expect($state.get('dashboard').templateUrl).to.equal(views.dashboard);
+        });
+
+        it('should map state dashboard to url / ', function() {
+            expect($state.href('dashboard', {})).to.equal('/');
+        });
+
+        it('of dashboard should work with $state.go', function() {
+            $state.go('dashboard');
+            $rootScope.$apply();
+            expect($state.is('dashboard'));
+        });
+    });
+});
